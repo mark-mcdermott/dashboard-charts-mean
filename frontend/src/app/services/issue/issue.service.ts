a939187fab762980fc4fdec9793ddb350ca372f8
Mark McDermott <mcdermottsolutions@gmail.com>
2018-07-07 09:32:32 -0500

    removed edit and create methods

--- frontend/src/app/services/issue/issue.service.ts
+++ frontend/src/app/services/issue/issue.service.ts
@@ -11,36 +11,8 @@ export class IssueService {
   constructor(private http: HttpClient) {
   }
 
-  addIssue(title, responsible, description, severity) {
-    const issue = {
-      title: title,
-      responsible: responsible,
-      description: description,
-      severity: severity
-    };
-    return this.http.post(`${this.uri}/issues/add`, issue);
-  }
-
   getIssues() {
     return this.http.get(`${this.uri}/issues`);
   }
 
-  getIssueById(id) {
-    return this.http.get(`${this.uri}/issues/${id}`);
-  }
-
-  updateIssue(id, title, responsible, description, severity, status) {
-    const issue = {
-      title: title,
-      responsible: responsible,
-      description: description,
-      severity: severity,
-      status: status
-    };
-    return this.http.post(`${this.uri}/issues/update/${id}`, issue);
-  }
-
-  deleteIssue(id) {
-    return this.http.get(`${this.uri}/issues/delete/${id}`);
-  }
 }
