a41f9589e288ad857837deb4fb99f0b403c6f3f5
Mark McDermott <mcdermottsolutions@gmail.com>
2018-08-12 21:04:28 -0500

    getting chart arrays from mongo

--- frontend/src/app/components/blood-pressure/blood-pressure.component.ts
+++ frontend/src/app/components/blood-pressure/blood-pressure.component.ts
@@ -18,23 +18,23 @@ export  class  BloodPressureComponent  implements  OnInit {
   // dataMap : any;
 
 
-  multi: any[];
-  //
-  view: any[] = [700, 400];
-  //
-  // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
-  //
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
+  // multi: any[];
+  // //
+  // view: any[] = [700, 400];
+  // //
+  // // options
+  // showXAxis = true;
+  // showYAxis = true;
+  // gradient = false;
+  // showLegend = true;
+  // showXAxisLabel = true;
+  // xAxisLabel = 'Country';
+  // showYAxisLabel = true;
+  // yAxisLabel = 'Population';
+  // //
+  // colorScheme = {
+  //   domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  // };
   //
   // constructor() {
   //   Object.assign(this, { single })
@@ -44,56 +44,17 @@ export  class  BloodPressureComponent  implements  OnInit {
   //   console.log(event);
   // }
 
-  private  bloodPressures:  Array<object> = [];
+  dateArr: any;
+  topArr: any;
+  bottomArr: any;
+  // options : any;
+  // onSelect : any;
+  // dataMap : any;
+
+  private  bloodPressureTable:  Array<object> = [];
   constructor(private bloodPressureService: BloodPressureService) {
 
-      Object.assign(this, { single })
-
-    // this.dataMap = {};
-    // let loadData = function(_data){
-    //   return new Promise(function(resolve,reject){
-    //     resolve(_data);
-    //   });
-    // }
-
-    // let CANDLE_DATA = [
-    //     ['7/26/17', 70, 70, 110, 110],
-    //     ['8/24/17', 78, 78, 112, 112],
-    //     ['11/1/17', 66, 66, 111, 111],
-    //     ['12/21/17', 80, 80, 126, 126],
-    //     ['3/13/18', 69, 69, 103, 103],
-    //     ['3/16/18', 73, 73, 124, 124],
-    //     ['4/18/18', 86, 86, 130, 130],
-    //     ['5/20/18', 67, 67, 129, 129],
-    //     ['5/22/18', 70, 70, 106, 106]
-    //   ];
-    //
-    // let CANDLE_OPTIONS = {
-    //     legend:'none',
-    //     candlestick: {
-    //       fallingColor: { strokeWidth: 0, fill: '#e74c3c' },
-    //       risingColor: { strokeWidth: 0, fill: '#e74c3c' }
-    //     },
-    //     hAxis: {
-    //       textStyle: {
-    //         color: '#666666'
-    //       }
-    //     },
-    //     vAxis: {
-    //       textStyle: {
-    //         color: '#666666'
-    //       }
-    //     },
-    //     width: ' 700'
-    //   };
-    //
-    // let CANDLE_CONFIG = {
-    //   firstRowAsData : true
-    // }
-    //
-    // this.dataMap['CANDLE_DATA'] =  loadData(CANDLE_DATA);
-    // this.dataMap['CANDLE_OPTIONS'] = CANDLE_OPTIONS;
-    // this.dataMap['CANDLE_CONFIG'] = CANDLE_CONFIG;
+      // this.getBloodPressures()
 
   }
   onSelect(event) {
@@ -101,11 +62,28 @@ export  class  BloodPressureComponent  implements  OnInit {
   }
   ngOnInit() {
     this.getBloodPressures();
+    //console.log(this.bloodPressures);
+    // for (let row in this.getBloodPressures) {
+    //   console.log(row)
+    //   // console.log(row.top)
+    //   // console.log(row.bottom)
+    //   // this.dates.push(row.date);
+    // }
   }
   public getBloodPressures(){
     this.bloodPressureService.getBloodPressures().subscribe((data: Array<object>) => {
-      // console.log(data);
-      this.bloodPressures = data;
+      this.bloodPressureTable = data;
+      this.dateArr = this.bloodPressureTable.map(bp => bp['date']);
+      this.topArr = this.bloodPressureTable.map(bp => bp['top']);
+      this.bottomArr = this.bloodPressureTable.map(bp => bp['bottom']);
     });
+
+    // this.bloodPressureService.getBloodPressures()
+    //     .then(bloodPressures => {
+    //    this.bloodPressures = bloodPressures;
+    //    console.log('this.users=' + this.bloodPressures);
+    // });
+    //
+    // });
   }
 }
